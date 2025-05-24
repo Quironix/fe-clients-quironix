6824507a60bdd831d2ebb9ed0ef64c7eca5c897d
Ignacio Norambuena <ignacionorambuenag@gmail.com>
2025-05-23 23:39:19 -0400

    Remove build directory from git tracking

--- build/static/chunks/src_app_onboarding_layout_tsx_08bbea04._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_86fa14e6._.js",
-  "static/chunks/node_modules_4e75390a._.js"
-],
-    source: "dynamic"
-});
--- build/static/chunks/src_app_onboarding_page_tsx_4684e10c._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_app_onboarding_step-personal-information_tsx_68f2d05f._.js"
-],
-    source: "dynamic"
-});
