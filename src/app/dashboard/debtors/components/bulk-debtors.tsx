1e178b5f827e7c072dced7b429b025d39675f3c9
Luis Andrade <fersorc12@gmail.com>
2025-06-16 10:54:29 -0400

    fix: update CSV download link in BulkDebtors component

--- src/app/dashboard/debtors/components/bulk-debtors.tsx
+++ src/app/dashboard/debtors/components/bulk-debtors.tsx
@@ -102,7 +102,7 @@ const BulkDebtors = () => {
           className="border-2 border-orange-500 bg-white text-orange-500 hover:bg-orange-500 hover:text-white"
           onClick={() => {
             window.open(
-              `https://storage.googleapis.com/dbt-latam.firebasestorage.app/debtors-csv.csv`,
+              `https://storage.googleapis.com/dbt-latam.firebasestorage.app/plantilla-carga-deudores.csv`,
               "_blank"
             );
           }}
